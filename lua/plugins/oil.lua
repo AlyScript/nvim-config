ce3bfdfe8359dc3d81b4ac51c530ddc5ab8d5821
adam <adamaly93@gmail.com>
2025-07-19 23:02:59 +0100

    Remove nvim-tree

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		keymaps = {
+			["<C-t>"] = "actions.select_tab", -- open file in new tab with Ctrl + t
+		},
+	},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = false,
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
